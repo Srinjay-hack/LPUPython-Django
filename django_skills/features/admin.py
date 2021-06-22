85688a96c8e117c67c4316cdf34e014ffea6b5b2
Srinjay-hack <59413151+Srinjay-hack@users.noreply.github.com>
2021-06-22 16:50:05 +0530

    Delete django_skills directory

--- django_skills/features/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from features import views
-# Register your models here.
-
-from features.models import Info
-
-
-
-admin.site.register(Info)
--- django_skills/features/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class Info(models.Model):
-
-    name=models.CharField(max_length=256)
-    contact=models.CharField(max_length=256)
-    interests=models.CharField(max_length=256)
